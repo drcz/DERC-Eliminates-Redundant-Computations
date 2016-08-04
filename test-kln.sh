314d0d91597f70f9003e13d1bae684cafa06afaf
derczyk <drcz@fnord>
2016-08-04 19:02:52 +0200

    readme

--- test-kln.sh
+++ test-kln.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 echo "kln (((^ (x) (* x x)) (+ 2 y))) y=3 [~23s]"
- (cat kln.kln ; echo "(names vals) ((y) (3))") | ./partial-evaluator.scm 
+(cat kln.kln ; echo "(names vals) ((y) (3))") | ./partial-evaluator.scm 
 echo "---"
+echo "make sure you have uncommented the dirty hack in peval-memory-and-stuff.scm!"
+echo ""
 echo "kln (((^ (x) (* x x)) (+ 2 y))) y=??? [~25s]"
- (cat kln.kln ; echo "(names) ((y))") | ./partial-evaluator.scm 
+(cat kln.kln ; echo "(names) ((y))") | ./partial-evaluator.scm 
+echo ""
+ 
